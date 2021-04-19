3e98aa2a080d27a5489c2c0fd94a90fcb29b5a5d
shawnwork <78519750+shawnwork@users.noreply.github.com>
2021-04-19 18:58:15 -0400

    Add files via upload

---
+++ release/deps/shell_rust.d
@@ -0,0 +1,5 @@
+/Users/sixiang/Downloads/8000_shell_rust-master 2/target/release/deps/shell_rust: src/main.rs
+
+/Users/sixiang/Downloads/8000_shell_rust-master 2/target/release/deps/shell_rust.d: src/main.rs
+
+src/main.rs:
---
+++ release/shell_rust.d
@@ -0,0 +1,1 @@
+/Users/sixiang/Downloads/8000_shell_rust-master\ 2/target/release/shell_rust: /Users/sixiang/Downloads/8000_shell_rust-master\ 2/src/main.rs
